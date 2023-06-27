2e5b5ae5601f68022063acdfe97fb3be6457cb42
godislove823543 <57354983+godislove823543@users.noreply.github.com>
2023-06-27 07:45:02 +0000

    commit

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
-
+from datetime import datetime
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "kiki"}
 
-#若要新增終端機 要按+來新增
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+#query parameter
+@app.get("/raspberry")
+async def read_item(time:str = datetime.now().strftime("%Y%m%d %H:%M:%S"),light: float = 0.0, temperature: float = 0.0):
+    
+    return {
+        "時間":time,
+        "光線":light,
+        "溫度":temperature
+    }
